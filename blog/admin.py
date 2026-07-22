42f596354bc87da81e743702ef6a94121c159665
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-07-22 22:07:14 +0300

    solved

---
+++ blog/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from blog.models import User, Post, Commentary
+
+
+@admin.register(User)
+class BlogUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ("title", "owner",)
+    list_filter = ("owner", "created_time",)
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    search_fields = ("user", "post",)
+    list_filter = ("user", "created_time",)
+
+
+admin.site.unregister(Group)
---
+++ blog/views.py
@@ -0,0 +1,46 @@
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.select_related(
+        "owner"
+    ).prefetch_related(
+        "comments"
+    )
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get("page")
+    page_obj = paginator.get_page(page)
+    context = {
+        "post_list": page_obj,
+        "page_obj": page_obj,
+    }
+    return render(request, "blog/index.html", context)
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = (
+        Post.objects.select_related(
+            "owner"
+        ).prefetch_related("comments").get(pk=pk)
+    )
+    new_comment = None
+    if request.user.is_authenticated and request.method == "POST":
+        comment_form = CommentaryForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.user = request.user
+            new_comment.save()
+    else:
+        comment_form = CommentaryForm()
+    context = {
+        "post": post,
+        "comment_form": comment_form,
+        "new_comment": new_comment,
+    }
+    return render(request, "blog/post_detail.html", context)
